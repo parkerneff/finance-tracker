7d48d080e1598dc6f7db1213862f246b610e6241
pneff <pneff@unicon.net>
2018-09-18 10:12:47 -0700

    end 195

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,12 +3,21 @@ class UsersController < ApplicationController
     @user = current_user
     @user_stocks = current_user.stocks
   end
+
   def my_friends
     @friendships = current_user.friends
   end
 
   def search
-    @users = User.search(params[:search_param])
-    render json: @users
+    if params[:search_param].blank?
+      flash.now[:danger] = "You have entered an empty search string"
+    else
+      @users = User.search(params[:search_param])
+      flash.now[:danger] = "No users found" if @users.blank?
+    end
+
+    respond_to do |format|
+      format.js {render partial: 'friends/result'}
+    end
   end
 end
